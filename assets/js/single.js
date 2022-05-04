b689d2199db235645d6a80f2ecdce3295116422e
vhivestate <garcia.valeria001@gmail.com>
2022-05-03 21:19:23 -0600

    add extra message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -8,14 +9,15 @@ var getRepoIssues = function(repo) {
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
-          response.json().then(function(data) {
-            // pass response data to dom function
-            displayIssues(data);
-          });
-        }
-        else {
-          alert("There was a problem with your request!");
-        }
+            response.json().then(function(data) {
+              displayIssues(data);
+          
+              // check if api has paginated issues
+              if (response.headers.get("Link")) {
+                console.log(displayWarning(repo));
+              }
+            });
+          }
       });
   };
 
@@ -24,7 +26,7 @@ var getRepoIssues = function(repo) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
       }
-      
+
     for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
@@ -54,5 +56,18 @@ var getRepoIssues = function(repo) {
         issueContainerEl.appendChild(issueEl);
       }
 };
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);  
+  };
   
   getRepoIssues("facebook/react");
